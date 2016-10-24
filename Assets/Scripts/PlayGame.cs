0eafa43fa8f2e7ef94af73ac65b6799273fac1c6
WILL SHELLABARGER <WNSHELL@gmail.com>
2016-10-24 13:55:45 -0400

    title screen

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+
+	// Update is called once per frame
+	void OnMouseEnter() {
+		GetComponent<Renderer>().material.color = Color.black;
+	}
+
+	void OnMouseExit() {
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+
+	void OnMouseUp()
+	{
+		SceneManager.LoadScene ("Level_1");
+	}
+}
---
+++ Assets/Scripts/mouseFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseFollow: MonoBehaviour {
+	private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		GetComponent<Rigidbody>().transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((target.y - transform.position.y), (target.x - transform.position.x))*Mathf.Rad2Deg - 90);
+
+	}
+}
